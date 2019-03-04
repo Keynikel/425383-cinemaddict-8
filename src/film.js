6e9b007f2a29774c508038578672fdda2b8613fe
MediaBayStudio <med.bay@yandex.ru>
2019-03-04 12:58:09 +0300

    1. Закоменнтированный код убрали. 2. Объединили функции создания карточек с формой управления и без нее. 3. Вынесли список фильтров в отдельный файл. 4. Для генерации карточек создали отдельный модуль. 5. Список фильмов генерируется только через функцию генерацию карточек.

--- src/film.js
+++ src/film.js
@@ -12,33 +12,30 @@ const MAX_COMMENTS = 40;
 const titles = [`Once Upon a Time in Hollywood`, `Joker`, `Dumbo`, `Zeroville`, `The Batman`, `Spider-Man: Far From Home`, `Aladdin`, `Dune`, `It: Chapter Two`, `Bond  25`, `The Lion King`, `Dark Phoenix`, `Avengers: Endgame`, `John Wick: Chapter 3 - Parabellum`, `Men in Black International`];
 const genres = [`Comedy`, `Thriller`, `Horror`, `Adventure `, `Drama`, `Crime`, `Sci-fi`];
 const posters = [`accused.jpg`, `blackmail.jpg`, `blue-blazes.jpg`, `fuga-da-new-york.jpg`, `moonrise.jpg`, `three-friends.jpg`];
-
-/* проверить, как еще это можно сделать*/
 const descriptionsFish = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
 /* Генерация описания фаильма. На вход подаем массив, в функции генерируем случайное количество предложений от 1 до 3 и выбираем в массиве случайные значения. Объедиянем в строку, в конце ее возвращаем*/
 const generateDescription = (descriptionArray) => {
- const amount = Math.round(additional.getRandomNumber(1, 3));
- let description = ``;
- for (let i = 1; i <= amount; i++) {
-   const key = Math.floor(additional.getRandomNumber(1, descriptionArray.length-1));
-   const frase = descriptionArray[key];
-   description += frase + '. ';
- }
- return description;
-}
+  const amount = Math.round(additional.getRandomNumber(1, 3));
+  let description = ``;
+  for (let i = 1; i <= amount; i++) {
+    const key = Math.floor(additional.getRandomNumber(1, descriptionArray.length - 1));
+    const frase = descriptionArray[key];
+    description += frase + `. `;
+  }
+  return description;
+};
 
 /*
-заголовок-случайный из массива titles
-рейтинг - число от MIN_RATING до MAX_RATING с точностью до десятых
-год - число от MIN_YEAR до MAX_YEAR
-длительность - часов от MIN_HOURS до MAX_HOURS, минут от 5 до 60
-жанр - случаный из массива genres
-постер - случайный из массива posters
+  заголовок-случайный из массива titles
+  рейтинг - число от MIN_RATING до MAX_RATING с точностью до десятых
+  год - число от MIN_YEAR до MAX_YEAR
+  длительность - часов от MIN_HOURS до MAX_HOURS, минут от 5 до 60
+  жанр - случаный из массива genres
+  постер - случайный из массива posters
 
-комментарии - случайное число от MIN_COMMENTS до MAX_COMMENTS,
-если 1 то подпись comment, любое другое значение - comments
-
+  комментарии - случайное число от MIN_COMMENTS до MAX_COMMENTS,
+  если 1 то подпись comment, любое другое значение - comments
 */
 export const getFilm = () => ({
   title: titles[Math.floor(Math.random() * titles.length)],
@@ -47,6 +44,6 @@ export const getFilm = () => ({
   duration: `${Math.floor(additional.getRandomNumber(MIN_HOURS, MAX_HOURS))}h ${Math.floor(additional.getRandomNumber(5, 60))}m`,
   genre: genres[Math.floor(Math.random() * genres.length)],
   poster: `./images/posters/${posters[Math.floor(Math.random() * posters.length)]}`,
-  description: generateDescription(descriptionsFish.split('. ')),
+  description: generateDescription(descriptionsFish.split(`. `)),
   comments: `${Math.floor(additional.getRandomNumber(MIN_COMMENTS, MAX_COMMENTS)) === 1 ? Math.floor(additional.getRandomNumber(MIN_COMMENTS, MAX_COMMENTS)) + ` comment` : Math.floor(additional.getRandomNumber(MIN_COMMENTS, MAX_COMMENTS)) + ` comments` }`
 });
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 /* Импорты */
+import {filtersList} from './filters';
 import {getFilter as makeFilter} from './make-filter';
-import {getFilmsMarkup, getCardNoControls as makeFilmCardWithoutControls} from './make-card';
-import * as additionl from './additional-functions';
 import {getFilm} from './film.js';
+import {generateCards} from './fill-container';
+import * as additionl from './additional-functions';
+
 
 /* Переменные */
 const MIN_CARDS = 1;
@@ -10,57 +12,9 @@ const MAX_CARDS = 10;
 const filtersContainer = document.querySelector(`.main-navigation`);
 const cardContainer = document.querySelector(`.films-list .films-list__container `);
 const topRatedContainer = document.querySelectorAll(`.films-list--extra .films-list__container`);
-const filtersList = [
-  {
-    'anchor': `all`,
-    'state': `active`,
-    'label': `All movies`
-  },
-  {
-    'anchor': `whatchlist`,
-    'label': `Whatchlist`,
-    'count': 13
-  },
-  {
-    'anchor': `history`,
-    'label': `History`,
-    'count': 4
-  },
-  {
-    'anchor': `favorites`,
-    'label': `Favorites`,
-    'count': 8
-  },
-  {
-    'anchor': `stats`,
-    'state': `additional`,
-    'label': `Stats`
-  }
-];
 const cardsList = []; // структура, которая описывает все карточки. Мы будем использовать для этого обычный массив.
+const topRatedCardsList = [];
 
-// const film = {
-//   'title': `The Assassination Of Jessie James By The Coward Robert Ford`,
-//   'rating': `9.8`,
-//   'year': `2018`,
-//   'duration': `1h&nbsp;13m`,
-//   'genre': `Comedy`,
-//   'poster': `./images/posters/three-friends.jpg`,
-//   'description': `A priest with a haunted past and a novice on the threshold of her final vows are sent by the Vatican to investigate the death of a young nun in Romania and confront a malevolent force in the form of a demonic nun.`,
-//   'comments': `13 comments`
-// };
-
-/* Функция для заполнения контейнера карточками по клику на фильтр. v2.0 для функции, которая работает с массивом карточек */
-
-export const generateCards = function (min, max, container) {
-  const cardsAmount = Math.floor(additionl.getRandomNumber(min, max));
-  const cardsList = [];
-  for (let i = 0; i < cardsAmount; i++) {
-    cardsList.push(getFilm());
-  }
-  additionl.clearField(container);
-  cardContainer.insertAdjacentHTML(`beforeEnd`, getFilmsMarkup(cardsList));
-};
 
 /* Очищаем блоки на странице*/
 additionl.clearField(filtersContainer);
@@ -78,20 +32,22 @@ filtersList.forEach(function (filter) {
 for (let i = 0; i < 7; i++) {
   cardsList.push(getFilm());
 }
+
+/* Генерируем карточки популярных фильмов*/
+for (let i = 0; i < 2; i++) {
+  topRatedCardsList.push(getFilm());
+}
+
 /* Наполняем карточками контейнер */
-cardContainer.insertAdjacentHTML(`beforeEnd`, getFilmsMarkup(cardsList));
-
-
-// topRatedContainer.forEach(function (extraContainer) {
-//   for (let i = 0; i < 7; i++) {
-//     extraContainer.insertAdjacentHTML(`beforeEnd`, makeFilmCardWithoutControls(film));
-//   }
-// });
+generateCards(7, 7, cardContainer, true);
+topRatedContainer.forEach(function (extraContainer) {
+  generateCards(2, 2, extraContainer, false);
+});
 
 /* Добавляем обработчики событий в фильтры */
 const filters = document.querySelectorAll(`.main-navigation__item `);
 filters.forEach(function (filter) {
   filter.addEventListener(`click`, function () {
-    generateCards(MIN_CARDS, MAX_CARDS, cardContainer);
+    generateCards(MIN_CARDS, MAX_CARDS, cardContainer, true);
   });
 });
